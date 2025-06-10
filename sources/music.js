81ef90aafd33e762f09c663930ba40506db5ec65
Isaac Trejo <isaacth114x@gmail.com>
2025-06-10 11:15:24 -0600

    Update music.js
    Creo que ya debería funcionar la música

--- sources/music.js
+++ sources/music.js
@@ -1,5 +1,5 @@
 function myFunction() {
-       fileArray = ["/sources/music/2004_Beat_2_EQ.mp3", "/sources/music/2004_Beat_3_EQ.mp3", "/sources/music/2004_Beat_4_EQ.mp3", "/sources/music/2004_Beat_5_EQ.mp3", "/sources/music/2004_Beat_6_EQ.mp3"];
+       fileArray = ["https://imisaac114x.github.io/undergroundspeedzone/sources/music/2004_Beat_2_EQ.mp3", "https://imisaac114x.github.io/undergroundspeedzone/sources/music/2004_Beat_3_EQ.mp3", "https://imisaac114x.github.io/undergroundspeedzone/sources/music/2004_Beat_4_EQ.mp3", "https://imisaac114x.github.io/undergroundspeedzone/sources/music/2004_Beat_5_EQ.mp3", "https://imisaac114x.github.io/undergroundspeedzone/sources/music/2004_Beat_6_EQ.mp3"];
        var num = Math.floor(Math.random() * fileArray.length);
        var x = document.getElementById("music");
        x.src = fileArray[num]; 
