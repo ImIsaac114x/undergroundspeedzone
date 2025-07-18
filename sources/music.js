3f950ae5e1d9c12efda6c643f46ab6ada2df3ab4
Isaac Trejo <isaacth114x@gmail.com>
2025-07-17 20:06:39 -0600

    Trying something new

--- sources/music.js
+++ sources/music.js
@@ -1,13 +1,1 @@
-function myFunction() {
-       fileArray = ["https://imisaac114x.github.io/undergroundspeedzone/sources/music/2004_Beat_2_EQ.mp3", "https://imisaac114x.github.io/undergroundspeedzone/sources/music/2004_Beat_3_EQ.mp3", "https://imisaac114x.github.io/undergroundspeedzone/sources/music/2004_Beat_4_EQ.mp3", "https://imisaac114x.github.io/undergroundspeedzone/sources/music/2004_Beat_5_EQ.mp3", "https://imisaac114x.github.io/undergroundspeedzone/sources/music/2004_Beat_6_EQ.mp3"];
-       var num = Math.floor(Math.random() * fileArray.length);
-       var x = document.getElementById("music");
-       x.src = fileArray[num]; 
-
-       var audio = document.getElementById("music");
-       audio.volume = 0.2;
-       audio.autoplay = true;
-       audio.load();
-    }
-
-    myFunction()
+var _0x228e=['https://imisaac114x.github.io/undergroundspeedzone/sources/music/2004_Beat_2_EQ.mp3','https://imisaac114x.github.io/undergroundspeedzone/sources/music/2004_Beat_3_EQ.mp3','https://imisaac114x.github.io/undergroundspeedzone/sources/music/2004_Beat_4_EQ.mp3','https://imisaac114x.github.io/undergroundspeedzone/sources/music/2004_Beat_5_EQ.mp3','https://imisaac114x.github.io/undergroundspeedzone/sources/music/2004_Beat_6_EQ.mp3','length','getElementById','src','volume','load'];(function(_0x168926,_0x228ed7){var _0x28f092=function(_0x173a7d){while(--_0x173a7d){_0x168926['push'](_0x168926['shift']());}};_0x28f092(++_0x228ed7);}(_0x228e,0x95));var _0x28f0=function(_0x168926,_0x228ed7){_0x168926=_0x168926-0x0;var _0x28f092=_0x228e[_0x168926];return _0x28f092;};function myFunction(){fileArray=[_0x28f0('0x0'),_0x28f0('0x1'),_0x28f0('0x2'),_0x28f0('0x3'),_0x28f0('0x4')];var _0x3b5f92=Math['floor'](Math['random']()*fileArray[_0x28f0('0x5')]);var _0x599014=document[_0x28f0('0x6')]('music');_0x599014[_0x28f0('0x7')]=fileArray[_0x3b5f92];var _0x38efdc=document['getElementById']('music');_0x38efdc[_0x28f0('0x8')]=0.2;_0x38efdc['autoplay']=!![];_0x38efdc[_0x28f0('0x9')]();}myFunction();
